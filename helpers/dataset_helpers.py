b29b292bc54539d06bb6b72909dd5053663e10da
Wissam <wessameljurdi@gmail.com>
2022-11-21 01:42:18 +0100

    Added the second experiment. Did several bug fixes (mainly to synthetic data experiments). Added group validation paper result files.

--- helpers/dataset_helpers.py
+++ helpers/dataset_helpers.py
@@ -53,33 +53,34 @@ def create_new_genre_records(user_df, modified_ratings, genres, movies, rating=0
     return records_to_add
 
 
-def flip_pofile_ratings(ratings_detailed, movies, target_cluster, N, users_thresh):
+def flip_pofile_ratings(ratings_detailed, movies, target_clusters, N, users_thresh):
     # make a copy of the original ratings df
     modified_ratings = ratings_detailed.copy()
     records_to_add = []
 
-    # threshold to consider for "+ve" feedback (rating >= 3 is considered +ve)
+    # threshold to consider for "+ve" feedback (rating >= like_threh is considered +ve)
     like_threh = 3
 
-    # users' ratings to modify (only take top k users from every target cluster)
-    cluster_df = ratings_detailed[ratings_detailed['cluster'] == target_cluster]
-    target_cluster_users = list(set(cluster_df.userId.to_list()))
-    target_users = target_cluster_users[:users_thresh]
+    for target_cluster in target_clusters:
+        # users' ratings to modify (only take top k users from every target cluster)
+        cluster_df = ratings_detailed[ratings_detailed['cluster'] == target_cluster]
+        target_cluster_users = list(set(cluster_df.userId.to_list()))
+        target_users = target_cluster_users[:users_thresh]
 
-    for target_user in target_users:
-        user_df = cluster_df[cluster_df['userId'] == target_user]
-        positive_df = user_df[user_df['rating'] >= like_threh]
-        negative_df = user_df[user_df['rating'] < like_threh]
+        for target_user in target_users:
+            user_df = cluster_df[cluster_df['userId'] == target_user]
+            positive_df = user_df[user_df['rating'] >= like_threh]
+            negative_df = user_df[user_df['rating'] < like_threh]
 
-        # get the genres of +ve and -ve items
-        positive_genres = get_all_genres_list(positive_df)
-        negative_genres = get_all_genres_list(negative_df)
+            # get the genres of +ve and -ve items
+            positive_genres = get_all_genres_list(positive_df)
+            negative_genres = get_all_genres_list(negative_df)
 
-        # create new records with opposite ratings for each genre type (+ve and -ve)
-        positive_records = create_new_genre_records(user_df, modified_ratings, positive_genres, movies, rating=0.5, N=N)
-        negative_records = create_new_genre_records(user_df, modified_ratings, negative_genres, movies, rating=5, N=N)
+            # create new records with opposite ratings for each genre type (+ve and -ve)
+            positive_records = create_new_genre_records(user_df, modified_ratings, positive_genres, movies, rating=0.5, N=N)
+            negative_records = create_new_genre_records(user_df, modified_ratings, negative_genres, movies, rating=5, N=N)
 
-        records_to_add.extend(positive_records + negative_records)
+            records_to_add.extend(positive_records + negative_records)
 
     new_records_df = pd.DataFrame(records_to_add)
     final_df = pd.concat([modified_ratings, new_records_df], ignore_index=True)
@@ -87,7 +88,7 @@ def flip_pofile_ratings(ratings_detailed, movies, target_cluster, N, users_thres
     return final_df[['userId', 'movieId', 'rating', 'timestamp']]
 
 
-def add_random_ratings(ratings, clusters, target_cluster, N, users_thresh):
+def add_random_ratings(ratings, clusters, target_clusters, N, users_thresh):
     # make a copy of the original ratings df
     modified_ratings = ratings.copy()
     records_to_add = []
@@ -95,31 +96,33 @@ def add_random_ratings(ratings, clusters, target_cluster, N, users_thresh):
     total_movies = len(list(set(ratings.movieId.to_list())))
     max_rating = max(list(set(ratings.rating.to_list())))
     min_rating = min(list(set(ratings.rating.to_list())))
-    # users' ratings to modify (only take top k users from every target cluster)
-    cluster_df = clusters[clusters['cluster'] == target_cluster]
-    target_cluster_users = list(set(cluster_df.userId.to_list()))
-    target_users = target_cluster_users[:users_thresh]
 
-    for target_user in target_users:
-        for n in range(N):
-            random_movie_id = np.random.choice(np.arange(1, total_movies, 1), size=1)[0]
-            random_rating = np.random.choice(np.arange(1, max_rating, min_rating), size=1)[0]
+    for target_cluster in target_clusters:
+        # users' ratings to modify (only take top k users from every target cluster)
+        cluster_df = clusters[clusters['cluster'] == target_cluster]
+        target_cluster_users = list(set(cluster_df.userId.to_list()))
+        target_users = target_cluster_users[:users_thresh]
 
-            # check if the random item has already been rated by the user.
-            # If so, no random record will be added to avoid duplicate ratings.
-            seen_movie = ratings[(ratings['userId'] == target_user) & (ratings['movieId'] == random_movie_id)]
-            if len(seen_movie) == 1:
-                continue
+        for target_user in target_users:
+            for n in range(N):
+                random_movie_id = np.random.choice(np.arange(1, total_movies, 1), size=1)[0]
+                random_rating = np.random.choice(np.arange(1, max_rating, min_rating), size=1)[0]
 
-            # new record to add
-            new_record = {
-                'userId': target_user,
-                'movieId': random_movie_id,
-                'rating': random_rating,
-                'timestamp': 1212603770
-            }
+                # check if the random item has already been rated by the user.
+                # If so, no random record will be added to avoid duplicate ratings.
+                seen_movie = ratings[(ratings['userId'] == target_user) & (ratings['movieId'] == random_movie_id)]
+                if len(seen_movie) == 1:
+                    continue
 
-            records_to_add.append(new_record)
+                # new record to add
+                new_record = {
+                    'userId': target_user,
+                    'movieId': random_movie_id,
+                    'rating': random_rating,
+                    'timestamp': 1212603770
+                }
+
+                records_to_add.append(new_record)
 
     new_records_df = pd.DataFrame(records_to_add)
     final_df = pd.concat([modified_ratings, new_records_df], ignore_index=True)
